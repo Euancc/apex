210dcfd6d574e2c0adff9c5ac229b8b3d3a9c5c9
Euan Cunningham <euan@Asus-Lap>
2024-02-28 06:14:29 +1100

    nav up

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,37 @@
 import React from 'react'
 import '../css/Navbar.css'
+import { HashLink as Link } from 'react-router-hash-link'
+import { BrowserRouter } from 'react-router-dom'
 
 function Navbar() {
   return (
-    <nav className="navbar">
-      <div>Navbar</div>
-    </nav>
+    <BrowserRouter>
+      <nav className="navbar">
+        <div className="nav-logo">Apex</div>
+        <ul className="nav-menu">
+          <li>
+            <Link to="about" className="nav-link">
+              About
+            </Link>
+          </li>
+          <li>
+            <Link to="services" className="nav-link">
+              Services
+            </Link>
+          </li>
+          <li>
+            <Link to="gallery" className="nav-link">
+              Gallery
+            </Link>
+          </li>
+          <li>
+            <Link to="contact" className="nav-link">
+              Contact
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </BrowserRouter>
   )
 }
 
