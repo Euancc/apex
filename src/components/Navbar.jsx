ec46cb149b7732cb74271469c02a549bf0ce699b
Euan Cunningham <euan@Asus-Lap>
2024-02-29 06:13:14 +1100

    mobile menu up

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../css/Navbar.css'
 import { HashLink as Link } from 'react-router-hash-link'
 import { BrowserRouter } from 'react-router-dom'
 
 function Navbar() {
+  const [click, setclick] = useState(false)
+  const handleClick = () => setclick(!click)
+  const closeMobileMenu = () => setclick(false)
+
   return (
     <BrowserRouter>
       <nav className="navbar">
         <div className="nav-logo">Apex</div>
-        <ul className="nav-menu">
+
+        <div className="menu-icon" onClick={handleClick}>
+          <img src={click ? 'close.png' : 'hamburger.png'} />
+        </div>
+
+        <ul className={click ? 'nav-menu active' : 'nav-menu'}>
           <li>
             <Link to="about" className="nav-link">
               About
